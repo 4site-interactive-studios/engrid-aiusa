e08624c022945c1e96fbca5d791791ff2defdbab
MichaelW@4Site <michaelw@4sitestudios.com>
2021-09-19 22:19:15 -0400

    initial port of rememberme

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,25 @@ const options: Options = {
   SkipToMainContentLink: true,
   SrcDefer: true,
   ProgressBar: true,
+  RememberMe: {
+    //remoteUrl: 'https://apps.4sitestudios.com/michaelw/engrid-aiusa-rememberme/iframe.html',
+    //remoteUrl: 'https://donate.amnestyusa.org/page/90391/action/1?mode=DEMO',
+    fieldOptInSelectorTarget: '.en__field.en__field--checkbox.en__field--871601.en__field--NOT_TAGGED_81',
+    fieldOptInSelectorTargetLocation: 'after',
+    fieldClearSelectorTarget: '.body-main > .en__component--copyblock .section-header',
+    fieldClearSelectorTargetLocation: 'after',
+    fieldNames: [
+      'supporter.firstName',
+      'supporter.lastName',
+      'supporter.address1',
+      'supporter.address2',
+      'supporter.city',
+      'supporter.country',
+      'supporter.region',
+      'supporter.postcode',
+      'supporter.emailAddress'
+    ]
+  },
   Debug: App.getUrlParameter('debug') == 'true' ? true : false,
   onLoad: () => console.log("Starter Theme Loaded"),
   onResize: () => console.log("Starter Theme Window Resized")
