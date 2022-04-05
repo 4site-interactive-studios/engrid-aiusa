1e547bac3b6c6cf96792b1f2718ccac82b897f56
Ry <57374659+cryptidgit@users.noreply.github.com>
2022-04-05 12:55:58 -0400

    Added engrid-script update

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, April 5, 2022 @ 11:57:49 ET
+ *  Date: Tuesday, April 5, 2022 @ 12:51:47 ET
  *  By: ryanoliver
  *  ENGrid styles: v0.10.12
  *  ENGrid scripts: v0.10.19
@@ -12763,7 +12763,7 @@ class Loader {
 
 
   reload() {
-    var _a, _b, _c;
+    var _a, _b, _c, _d;
 
     const isLoaded = engrid_ENGrid.getBodyData("loaded");
     let assets = this.getOption("assets");
@@ -12779,7 +12779,7 @@ class Loader {
     // Fetch the desired repo, assets location, and override JS/CSS
 
     const engrid_repo = this.getOption("repo-name");
-    const engrid_repo_owner = this.getOption("repo-owner");
+    const engrid_repo_owner = (_a = this.getOption("repo-owner")) !== null && _a !== void 0 ? _a : "4site-interactive-studios";
     let engrid_js_url = "";
     let engrid_css_url = "";
 
@@ -12792,8 +12792,8 @@ class Loader {
           engrid_js_url = `https://engrid-${theme}.test/dist/engrid.js`;
           engrid_css_url = `https://engrid-${theme}.test/dist/engrid.css`;
         } else {
-          engrid_js_url = `https://engrid-${engrid_repo}.test/dist/engrid.js`;
-          engrid_css_url = `https://engrid-${engrid_repo}.test/dist/engrid.css`;
+          engrid_js_url = `https://${engrid_repo}.test/dist/engrid.js`;
+          engrid_css_url = `https://${engrid_repo}.test/dist/engrid.css`;
         }
 
         break;
@@ -12801,10 +12801,10 @@ class Loader {
       case "flush":
         this.logger.log("FLUSHING CACHE");
         const timestamp = Date.now();
-        const jsCurrentURL = new URL(((_a = this.jsElement) === null || _a === void 0 ? void 0 : _a.getAttribute("src")) || "");
+        const jsCurrentURL = new URL(((_b = this.jsElement) === null || _b === void 0 ? void 0 : _b.getAttribute("src")) || "");
         jsCurrentURL.searchParams.set("v", timestamp.toString());
         engrid_js_url = jsCurrentURL.toString();
-        const cssCurrentURL = new URL(((_b = this.cssElement) === null || _b === void 0 ? void 0 : _b.getAttribute("href")) || "");
+        const cssCurrentURL = new URL(((_c = this.cssElement) === null || _c === void 0 ? void 0 : _c.getAttribute("href")) || "");
         cssCurrentURL.searchParams.set("v", timestamp.toString());
         engrid_css_url = cssCurrentURL.toString();
         break;
@@ -12817,7 +12817,7 @@ class Loader {
 
     this.setCssFile(engrid_css_url);
     this.setJsFile(engrid_js_url);
-    (_c = this.jsElement) === null || _c === void 0 ? void 0 : _c.remove();
+    (_d = this.jsElement) === null || _d === void 0 ? void 0 : _d.remove();
     return true;
   }
 
