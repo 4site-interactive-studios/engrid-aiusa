58caf17ed6ded6b16997582dafa035eada993f44
Ry <57374659+cryptidgit@users.noreply.github.com>
2022-08-17 09:30:47 -0400

    Moved candle from button edge of on mobile

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, August 15, 2022 @ 13:28:12 ET
+ *  Date: Wednesday, August 17, 2022 @ 09:29:59 ET
  *  By: ryanoliver
  *  ENGrid styles: v0.13.13
  *  ENGrid scripts: v0.13.14
