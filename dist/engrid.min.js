bebabaa5538900845fdeee8ddb6c33001981d8d7
bryancasler <bryan.casler@gmail.com>
2024-02-21 15:05:39 -0500

    More upstream styling

--- dist/engrid.min.js
+++ dist/engrid.min.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Wednesday, February 21, 2024 @ 14:26:07 ET
+ *  Date: Wednesday, February 21, 2024 @ 15:01:44 ET
  *  By: bryancasler
  *  ENGrid styles: v0.16.11
  *  ENGrid scripts: v0.16.11
