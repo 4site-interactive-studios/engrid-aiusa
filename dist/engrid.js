c99fcbaf6d3a943382cff473d0d659b194050950
Fernando Santos <fern@ndo.io>
2025-04-25 01:56:37 -0400

    Last Style Tweaks for PostDonationEmbed

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Friday, April 25, 2025 @ 24:57:16 ET
+ *  Date: Friday, April 25, 2025 @ 01:15:00 ET
  *  By: fernando
  *  ENGrid styles: v0.21.9
  *  ENGrid scripts: v0.21.11
